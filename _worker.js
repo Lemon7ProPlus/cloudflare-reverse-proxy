5cd26c0c4d574241dce0eb859512388aa17c39a6
Time <33477685+Lemon7ProPlus@users.noreply.github.com>
2024-06-07 10:53:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,31 +1,62 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request) {
   const url = new URL(request.url);
-
+  let actualUrlStr = url.pathname.replace("/proxy/", "");
+  actualUrlStr = decodeURIComponent(actualUrlStr);  // Decode the URI component
   // 新增：检查用户是否直接访问代理地址
   if (url.pathname === '/' || url.pathname === '/proxy/') {
     return createLandingPage();
+   }
+  // Copy headers from the original request, but remove any CF headers
+  let newHeaders = new Headers();
+  for (let pair of request.headers.entries()) {
+    if (!pair[0].startsWith('cf-')) {
+      newHeaders.append(pair[0], pair[1]);
+    }
   }
 
-  const actualUrlStr = url.pathname.replace("/proxy/","") + url.search + url.hash
-
-  const actualUrl = new URL(actualUrlStr)
-
-  const modifiedRequest = new Request(actualUrl, {
-    headers: request.headers,
+  const modifiedRequest = new Request(actualUrlStr, {
+    headers: newHeaders,
     method: request.method,
     body: request.body,
-    redirect: 'follow'
+    redirect: 'manual'
   });
 
   const response = await fetch(modifiedRequest);
-  const modifiedResponse = new Response(response.body, response);
+  let modifiedResponse;
+  let body = response.body;
 
-  // 添加允许跨域访问的响应头
+  // Handle redirects
+  if ([301, 302, 303, 307, 308].includes(response.status)) {
+    const location = new URL(response.headers.get('location'));
+    const modifiedLocation = "/proxy/" + encodeURIComponent(location.toString());
+    modifiedResponse = new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText
+    });
+    modifiedResponse.headers.set('Location', modifiedLocation);
+  } else {
+    if (response.headers.get("Content-Type") && response.headers.get("Content-Type").includes("text/html")) {
+      const originalText = await response.text();
+      const regex = new RegExp('((href|src|action)=["\'])/(?!/)', 'g');
+      const modifiedText = originalText.replace(regex, `$1${url.protocol}//${url.host}/proxy/${encodeURIComponent(new URL(actualUrlStr).origin + "/")}`);
+      body = modifiedText;
+    }
+
+    modifiedResponse = new Response(body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers
+    });
+  }
+
+  // Add CORS headers
   modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+  modifiedResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  modifiedResponse.headers.set('Access-Control-Allow-Headers', '*');
 
   return modifiedResponse;
 }
